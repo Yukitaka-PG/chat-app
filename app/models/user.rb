703f2b1defd26a9614e240b38fa69fdb3118cc42
Yukitaka-PG <alliswellthatendwell0@gmail.com>
2021-02-03 17:28:42 +0900

    Revert "ユーザー認証機能の実装"
    This reverts commit 938a6f54d5b83b72db79ec2bd4f986983ce1538d.

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+end
