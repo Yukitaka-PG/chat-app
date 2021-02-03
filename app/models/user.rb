4b1bed962ac5dba0846882a1d23edb86bfd277a4
Yukitaka-PG <alliswellthatendwell0@gmail.com>
2021-02-03 18:02:16 +0900

    ユーザー編集機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  private
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-  end
 end
